b1a957f6785ec72d7da1f586035efcdc3538d12b
Shahriyar Sheikh <m.shahriyarsheikh@gmail.com>
2018-09-26 16:35:51 +0500

    Added registration for a renter in main contract

--- Smart Contacts/RentalMainContract.sol
+++ Smart Contacts/RentalMainContract.sol
@@ -11,6 +11,7 @@ contract RentalMainContract{
     // Events
     event RentOfferPlaced(address indexed caller, bytes32 indexed rentOfferHash);
     event RentOfferModified(address indexed caller, bytes32 indexed rentOfferHash);
+    event RegisterRenter(address indexed caller, bool indexed registrationStatus);
     
     //Models
     struct RentalOffer{
@@ -43,11 +44,32 @@ contract RentalMainContract{
     //Stores all the details of the registered renter;
     mapping(address=> RegisteredRenter) registeredRenter;
    
+   
     constructor(address tokenAddress, address hotWalletAddress, uint commissionFees) public{
         _ownerOfContract = msg.sender;
         _tokenAddress = tokenAddress;
         _hotWalletAddress = hotWalletAddress;
         _commissionFees = commissionFees;
+    }
+    
+    
+    
+    
+    function registerRenter(address renterAddress, bytes32 renterDetailsHash) external{
+        
+        //This is useless i think, just need to ensure that the user cannot register someone else.
+        require(renterAddress == msg.sender);
+        
+        //user allready registered
+        if(registeredRenter[renterAddress].addressOfRenter == msg.sender){
+            emit RegisterRenter(msg.sender,false);
+            revert();
+        }else{
+            registeredRenter[renterAddress].addressOfRenter = msg.sender;
+            registeredRenter[renterAddress].renterDetails = renterDetailsHash;
+            emit RegisterRenter(msg.sender,true);
+        }
+        
     }
     
     function placeRentOffer(uint offeredQuantity, 
@@ -60,20 +82,20 @@ contract RentalMainContract{
         //TO CHECK IF THE OFFER IS PLACED by a registerd renter                        
         require(registeredRenter[msg.sender].addressOfRenter == msg.sender);
         
-        bytes32 rentOfferHash = keccak256(abi.encodePacked(msg.sender, now, offeredQuantity, startDate, endDate,placeDetailsHash, "Sell"));
-        
-        //If Same order is allready placed then it cannot be placed again
-        if(rentals[rentOfferHash].renterAddress != address(0)) revert();
-       
-        
         //Date validations
         require(startDate > now && now < endDate);
         if(startDate >= endDate ) revert();
         
+        bytes32 rentOfferHash = keccak256(abi.encodePacked(msg.sender, offeredQuantity, startDate, endDate,placeDetailsHash, "Sell"));
+        
+        //If Same order is allready placed then it cannot be placed again
+        if(rentals[rentOfferHash].placeDetailsHash == placeDetailsHash) revert();
+       
         rentals[rentOfferHash].offeredQuantity = offeredQuantity;
         rentals[rentOfferHash].renterAddress = msg.sender;
         rentals[rentOfferHash].arbitratorAddress = arbitratorAddress;
         rentals[rentOfferHash].arbitratorFee = arbitratorFees;
+        rentals[rentOfferHash].placeDetailsHash = placeDetailsHash;
         
         //An array that keep tracks of the current rent offers by hash
         currentRentOffersHash.push(rentOfferHash);
@@ -89,7 +111,6 @@ contract RentalMainContract{
                              uint startDate, 
                              uint endDate,
                              address arbitratorAddress, 
-                             
                              bytes32 placeDetailsHash,
                              uint arbitratorFees) external {
         
@@ -99,13 +120,14 @@ contract RentalMainContract{
         //To check whether the place is already rented
         require(rentalsTaken[rentedOfferHash].length == 0);
         
-        bytes32 newRentedOfferHash = keccak256(abi.encodePacked(msg.sender, now, offeredQuantity, address(0), startDate, endDate,placeDetailsHash, "Modify"));
+        bytes32 newRentedOfferHash = keccak256(abi.encodePacked(msg.sender, offeredQuantity, address(0), startDate, endDate,placeDetailsHash, "Modify"));
             
         RentalOffer storage rentOffer = rentals[newRentedOfferHash];
         rentOffer.offeredQuantity = offeredQuantity;
         rentOffer.renterAddress = msg.sender;
         rentOffer.arbitratorAddress = arbitratorAddress;
         rentOffer.arbitratorFee = arbitratorFees;
+        rentOffer.placeDetailsHash = newRentedOfferHash;
         currentRentOffersHash[rentOfferIndexes[rentedOfferHash]] = newRentedOfferHash;
         
         emit RentOfferModified(msg.sender,newRentedOfferHash);
